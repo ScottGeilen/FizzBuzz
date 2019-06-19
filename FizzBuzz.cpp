af3524b0f70adcd17c79df02d0c0ca8a58ee8e73
Scottacious <scottgeilen99@gmail.com>
2019-06-19 17:51:42 -0400

    Added FizzBuzz.py

---
+++ FizzBuzz.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Declaring num as int
+    int num;
+    
+    for (num = 1; num <= 100; num++)
+    {
+        if (num%3 == 0)
+            cout << "Fizz" << endl;
+    }
+    
+
+}
